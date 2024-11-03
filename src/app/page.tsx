e6fbef52e5d3534e25746b002395e86f5c92f8b3
Karpenoid <mxkarpen@gmail.com>
2024-11-03 21:44:57 +0200

    Fix lint errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import { Sidebar } from 'lucide-react';
-import { SidebarProvider, SidebarTrigger } from '@/src/components/ui/sidebar';
-import { AppSidebar } from '@/src/components/AppSideBar/AppSideBar';
-
 export default function Home() {
   return (
     <div>
-      {/*<SidebarProvider>*/}
-      {/*    <AppSidebar />*/}
-      {/*</SidebarProvider>*/}
+      <p>Main Page</p>
     </div>
   );
 }
