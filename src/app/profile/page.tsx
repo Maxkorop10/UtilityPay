e4fd1c5ea67f3f294950ad1a83049216b290e9c2
Karpenoid <mxkarpen@gmail.com>
2024-11-11 17:45:33 +0200

    Add husky, fix tailwind.config

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,37 +1,11 @@
 import { Label } from '@radix-ui/react-label';
-import { Button } from '@/src/components/ui/button';
+import ProfileInfo from '@/src/modules/profile-info/profile-info';
 
 export default function ProfilePage() {
   return (
     <div className="rounded-[10px] shadow-md h-fit w-[100%] p-4 bg-white">
       <Label className="font-bold text-2xl">Особистий профіль</Label>
-
-      <div className="flex flex-col mt-4">
-        <Label className="text-gray-700">ПІБ</Label>
-        <Label className="text-gray-500">Прізвище Ім&apos;я Побатькові</Label>
-      </div>
-
-      <div className="flex flex-col mt-4">
-        <Label className="text-gray-700">Адреса</Label>
-        <Label className="text-gray-500">
-          м. Черкаси, вул. Благовісна, буд. 31, кв. 121
-        </Label>
-      </div>
-
-      <div className="flex flex-col mt-4">
-        <Label className="text-gray-700">Номер телефону</Label>
-        <Label className="text-gray-500">+380663512563</Label>
-      </div>
-
-      <div className="flex flex-col mt-4">
-        <Label className="text-gray-700">Номер особового рахунку</Label>
-        <Label className="text-gray-500">4897 5132 4529 5014</Label>
-      </div>
-
-      <div className="mt-7 flex gap-7">
-        <Button className="bg-blue-500">Оновити дані</Button>
-        <Button>Вийти</Button>
-      </div>
+      <ProfileInfo />
     </div>
   );
 }
--- src/app/utility-bills/page.tsx
+++ src/app/utility-bills/page.tsx
@@ -2,45 +2,38 @@ import { Label } from '@radix-ui/react-label';
 import { Zap, Flame, Droplet, Fence, Trash2 } from 'lucide-react';
 import Utility from '@/src/modules/utility-service/utility';
 
+const utilitiesData = [
+  {
+    Icon: Flame,
+    utility_name: 'Газопостачання',
+    used_value: '14',
+    price: '210',
+  },
+  {
+    Icon: Droplet,
+    utility_name: 'Водопостачання',
+    used_value: '10',
+    price: '150',
+  },
+  { Icon: Zap, utility_name: 'Світло', used_value: '5', price: '100' },
+  { Icon: Fence, utility_name: 'Опалення', used_value: '7', price: '120' },
+  { Icon: Trash2, utility_name: 'Вивіз сміття', used_value: '2', price: '60' },
+];
+
 export default function UtilityBillsPage() {
   return (
     <div className="rounded-[10px] shadow-md h-fit w-[100%] p-4 bg-white">
       <Label className="font-bold text-2xl">Комунальні послуги</Label>
 
-      <Utility
-        Icon={Flame}
-        utility_name={'Газопостачання'}
-        used_value={'14'}
-        price={'210'}
-      />
-
-      <Utility
-        Icon={Droplet}
-        utility_name={'Водопостачання'}
-        used_value={'10'}
-        price={'150'}
-      />
-
-      <Utility
-        Icon={Zap}
-        utility_name={'Світло'}
-        used_value={'5'}
-        price={'100'}
-      />
-
-      <Utility
-        Icon={Fence}
-        utility_name={'Опалення'}
-        used_value={'7'}
-        price={'120'}
-      />
-
-      <Utility
-        Icon={Trash2}
-        utility_name={'Вивіз сміття'}
-        used_value={'2'}
-        price={'60'}
-      />
+      {utilitiesData.map((utility, index) => (
+        <Utility
+          key={index}
+          Icon={utility.Icon}
+          utility_name={utility.utility_name}
+          used_value={utility.used_value}
+          price={utility.price}
+        />
+      ))}
     </div>
   );
 }
