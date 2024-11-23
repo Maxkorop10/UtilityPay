a1de63966268c8369536f4eba07dc6dd33601fc5
Karpenoid <mxkarpen@gmail.com>
2024-11-23 15:57:58 +0200

    Changed font size.

--- src/modules/utility-service/utility.tsx
+++ src/modules/utility-service/utility.tsx
@@ -13,16 +13,16 @@ const Utility: FC<UtilityProps> = ({
     <div className="my-4 py-4 bg-white rounded-lg border-b">
       <div className="flex items-center justify-between">
         <div className="flex flex-col text-gray-800 gap-2">
-          <div className="flex items-center gap-3">
-            <Icon size={32} color={'#1F2937'} />
-            <Label className="font-semibold text-xl">{utility_name}</Label>
+          <div className="flex items-center gap-2">
+            <Icon size={22} color={'#1F2937'} />
+            <Label className="font-semibold text-[20px] leading-[22px]">{utility_name}</Label>
           </div>
           <Label className="text-sm text-gray-500">
             Спожита кількість: <span className="font-medium">{used_value}</span>
           </Label>
         </div>
         <div className="flex flex-col gap-2">
-          <Label className="font-semibold text-xl">
+          <Label className="font-semibold text-sm text-right">
             Сума: <span className="font-medium">{price} грн.</span>
           </Label>
           <Button className="bg-blue-600 text-white rounded-lg px-5 py-2 hover:bg-blue-700 transition-colors duration-200">
