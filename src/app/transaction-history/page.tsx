455d929a7a8e4199de1107ac2ec4883d56232c0e
VladasServant <bigdenclub@gmail.com>
2024-11-23 15:06:02 +0200

    cart + trans history add

---
+++ src/app/transaction-history/page.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from 'next';
+import { Label } from '@radix-ui/react-label';
+import Transaction from '@/src/modules/transaction-history-service/history'
+
+export const metadata: Metadata = {
+  title: 'Історія перекази',
+};
+
+const TransactionHistoryData = [
+  {
+    serviceName: 'Оплата комунальних',
+    price: '250 грн',
+    date: '2024-11-20',
+    
+  },
+  {
+    serviceName: 'Мобільний зв’язок',
+    price: '100 грн',
+    date: '2024-11-18',
+  },
+  {
+    serviceName: 'Інтернет',
+    price: '200 грн',
+    date: '2024-11-15',
+  },
+];
+
+export default function TransactionHistoryPage() {
+  return (
+    <div className="rounded-[10px] shadow-md h-fit w-[100%] p-4 bg-white">
+      <Label className="font-bold text-2xl mb-4">Історія переказів</Label>
+      <ul className="space-y-4">
+        {TransactionHistoryData.map((transaction, index) => (
+          <Transaction
+            key={index}
+            serviceName={transaction.serviceName}
+            price={transaction.price}
+            date={transaction.date}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ src/modules/transaction-history-service/types.ts
@@ -0,0 +1,5 @@
+export interface HistoryProps {
+    serviceName: string;
+    price: string;
+    date: string;
+}
